98ac28dcdb843ce234875d357d417c0a2723e0c5
Craig Eisen <Ceisen91@gmail.com>
2016-04-14 15:37:06 -0400

    added code to connect to mongo lab

--- db/connection.js
+++ db/connection.js
@@ -10,4 +10,11 @@ var CrazyUrlSchema = new mongoose.Schema(
 mongoose.model("CrazyUrl", CrazyUrlSchema);
 // mongoose.connect("mongodb://localhost/crazy-url-shortener");
 
+
+if(process.env.NODE_ENV == "production"){
+  mongoose.connect(process.env.MONGOLAB_URI);
+}else{
+  mongoose.connect("mongodb://localhost/whenpresident");
+}
+
 module.exports = mongoose;
